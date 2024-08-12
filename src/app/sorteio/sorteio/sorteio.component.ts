728c5e68e24acce349294c4b62b85a3ded3e8021
Danilo Lima <danilo.slima@hotmail.com>
2024-08-12 19:50:57 -0300

    02 - Organizando sorteio e Estilizando CSS

--- src/app/sorteio/sorteio/sorteio.component.ts
+++ src/app/sorteio/sorteio/sorteio.component.ts
@@ -16,27 +16,34 @@ export class SorteioComponent implements OnInit {
     { nome: 'Gilson', numero: 2, foto: 'assets/gilson.png', selecionado: false },
     { nome: 'Galudo', numero: 3, foto: 'assets/galudo.png', selecionado: false },
     { nome: 'Juninho', numero: 4, foto: 'assets/juninho.png', selecionado: false },
-    { nome: 'Lucas Oliveira', numero: 5, foto: 'link_da_foto_5', selecionado: false },
-    { nome: 'Landro', numero: 6, foto: 'link_da_foto_6', selecionado: false },
-    { nome: 'Gabriel', numero: 7, foto: 'link_da_foto_7', selecionado: false },
-    { nome: 'Mickael', numero: 8, foto: 'link_da_foto_8', selecionado: false },
+    { nome: 'Lucas Oliveira', numero: 5, foto: 'assets/lucaso.png', selecionado: false },
+    { nome: 'Leandro', numero: 6, foto: 'assets/leandro.png', selecionado: false },
+    { nome: 'Gabriel', numero: 7, foto: 'assets/gabriel.png', selecionado: false },
+    { nome: 'Mickael', numero: 8, foto: 'assets/mickael.png', selecionado: false },
     { nome: 'Bruno Lima', numero: 9, foto: 'assets/BrunoLima.png', selecionado: false },
-    { nome: 'Luis Carlos', numero: 10, foto: 'link_da_foto_10', selecionado: false },
-    { nome: 'Perfeito', numero: 11, foto: 'link_da_foto_11', selecionado: false },
-    { nome: 'Thiago K', numero: 12, foto: 'link_da_foto_12', selecionado: false },
-    { nome: 'Thiago Carvalho', numero: 13, foto: 'link_da_foto_13', selecionado: false },
-    { nome: 'Renan', numero: 14, foto: 'link_da_foto_14', selecionado: false },
-    { nome: 'Paulo Filipini', numero: 15, foto: 'link_da_foto_15', selecionado: false },
-    { nome: 'Nikolas', numero: 16, foto: 'link_da_foto_16', selecionado: false },
-    { nome: 'Natasha', numero: 17, foto: 'link_da_foto_17', selecionado: false },
-    
-  ];
+    { nome: 'Luis Carlos', numero: 10, foto: 'assets/luis.png', selecionado: false },
+    { nome: 'Perfeito', numero: 11, foto: 'assets/perfeito.png', selecionado: false },
+    { nome: 'Natasha', numero: 12, foto: 'assets/natasha.png', selecionado: false },
+    { nome: 'Thiago Carvalho', numero: 13, foto: 'assets/thiagoc.png', selecionado: false },
+    { nome: 'Renan', numero: 14, foto: 'assets/renan.png', selecionado: false },
+    { nome: 'Paulo Filipini', numero: 15, foto: 'assets/paulo.png', selecionado: false },
+    { nome: 'Nikolas', numero: 16, foto: 'assets/nikolas.png', selecionado: false },
+    { nome: '1 Sandro', numero: 'Goleiro', foto: 'assets/sandro.png', selecionado: false },
+    { nome: '2 Jorge', numero: 'Goleiro', foto: 'assets/jorge.png', selecionado: false },
+  ]
+
+  
 
   times: any[] = [];
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+
+  
+  this.jogadores = this.jogadores.sort((a, b) => a.nome.localeCompare(b.nome));//ordene por nome
+
+   }
 
   toggleCheckbox(index: number) {
     this.jogadores[index].selecionado = !this.jogadores[index].selecionado;
@@ -50,8 +57,10 @@ export class SorteioComponent implements OnInit {
   }
 
   sortearTimes() {
+
     const jogadoresSelecionados = this.jogadores.filter(jogador => jogador.selecionado);
     const jogadoresEmbaralhados = [...jogadoresSelecionados].sort(() => 0.5 - Math.random());
+
     this.times = [];
 
     while (jogadoresEmbaralhados.length) {
